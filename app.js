816c082f63c925a9f742a4698a8d7a2ca3db2cc5
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-05-10 23:47:39 +0530

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const exressLayouts = require('express-ejs-layouts');
+
+app.use(express.static("public"))
+app.use(express.static("images"))
+app.use(exressLayouts);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"))
+app.use(express.static("image"))
+
+
+
+app.use('/gallery/', (req, res) => res.render('gallery'));
+app.use('/events/', (req, res) => res.render('events'));
+app.use('/covid_awarness/', (req, res) => res.render('covid'));
+app.use('/signin/', (req, res) => res.render('signin'));
+
+//Home
+app.get("/", function (req, res) {
+    res.render("home");
+});
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on PORT:` + PORT));
