e3d03cc5a47e7711f4ae22d2791404189cdfa377
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-05-12 19:22:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const exressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
 app.set("view engine", "ejs");
 
 app.use(express.static("public"))
 app.use(express.static("images"))
 var bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 app.use(exressLayouts);
 var ContactUs = require("./models/contactUs");
@@ -14,12 +14,6 @@ var db ="mongodb+srv://nimitha:nimitha@cluster0.kbbl4.mongodb.net/myFirstDatabas
 mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
-
-
-app.set("view engine", "ejs");
-app.use(express.static("public"))
-app.use(express.static("image"))
-var jsonParser = bodyParser.json()
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -47,9 +41,9 @@ app.use('/success/',(req,res)=>res.render('success'))
 app.get("/", function (req, res) {
     res.render("home");
 });
-// app.all('*', (req, res, next) => {
-//     next(new ExpressError('Page Not Found', 404))
-// });
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+});
 
 const PORT = process.env.PORT || 3000;
 
